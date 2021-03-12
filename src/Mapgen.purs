139d9ba897298b1de3912c36b347a6aae8f17619
redxaxder <abyaly@gmail.com>
2021-03-11 22:53:56 -0600

    toward mapgen

--- src/Mapgen.purs
+++ src/Mapgen.purs
@@ -8,6 +8,10 @@ import Random as R
 import Data.Traversable (scanl)
 import Data.FunctorWithIndex (mapWithIndex)
 
+import Control.Monad.Rec.Class (Step (..), tailRecM)
+
+import Partial.Unsafe (unsafePartial)
+
 type Block =
   { x:: Int, y:: Int, width:: Int, height:: Int }
 
@@ -15,39 +19,43 @@ type Room = Array Block
 
 {-
 
-step 1
+[x] step 1
   subdivide rectangle
    - cut up large block into smaller blocks
 
-step 2
+[x] step 2
   - recursively do this again until they are all small enough
 
-step 3
+[ ] step 2.5
+  - determine block adjacency
+
+[ ] step 3
   - merge small blocks to make them bigger
   - this creates rooms
 
-step 4
+[ ] step 4
   - generate blobs
   - each blob is generated so it doesn't cut a room
 
-step 5
+[ ] step 5
   place entrance and exit on edge of map
     (and not in the same room)
 
-step 6
+[ ] step 6
   connect rooms such that
   - there is a path from start to end
   - it doesn't cross too few rooms
   - or too many rooms
 
-step 7
+[ ] step 7
   - connect some other rooms at random
     - but don't shorten the path
 
 -}
 
+-- start is the position of the first interior spot
+-- len is the number of interior spots
 type Interval = { start :: Int, len :: Int }
-
 
 partition :: Int -> Int -> Random (Array Int)
 partition min n =
@@ -73,7 +81,6 @@ partition min n =
          let remainder = s - split1 - split2
          R.shuffle [split1,split2,remainder]
 
-
 subdivideInterval :: Int -> Interval -> Random (Array Interval)
 subdivideInterval minSize {start,len} = do
   lens <- partition minSize len
@@ -92,7 +99,89 @@ subdivide minSize {x,width,y,height} = do
   let mkBlock xi yi = { x: xi.start, y: yi.start, width: xi.len, height: yi.len}
   pure $ mkBlock <$> xIntervals <*> yIntervals
 
+tooBig :: Int -> Block -> Boolean
+tooBig m {width, height} = width > m || height > m
 
+
+type SubdivWip = {complete :: Array Block, incomplete :: Array Block}
+recursivelySubdivide :: Int -> Int -> Block -> Random (Array Block)
+recursivelySubdivide minSize maxSize block = tailRecM go
+  { incomplete: [block]
+  , complete: []
+  }
+  where
+    go :: SubdivWip -> Random (Step SubdivWip (Array Block))
+    go {complete, incomplete} =
+         let {no,yes} = Array.partition (tooBig maxSize) incomplete
+             completeNew = complete <> no
+          in if Array.null yes
+             then pure $ Done completeNew
+             else do
+               arrayOfArrays <- for yes (subdivide minSize)
+               let incompleteNew = Array.concat arrayOfArrays
+               pure $ Loop 
+                 { complete: completeNew
+                 , incomplete: incompleteNew
+                 }
+
+unproduct :: Block -> {h :: Interval, v :: Interval}
+unproduct {x,y,width,height} =
+  { h: { start: x, len: width }
+  , v: { start: y, len: height }
+  }
+
+areAdjacent :: Block -> Block -> Boolean
+areAdjacent a b =
+  let sa = unproduct a
+      sb = unproduct b
+   in     (areKissing sa.h sb.h && not (disjoint sa.v sb.v))
+       || (areKissing sa.v sb.v && not (disjoint sa.h sb.h))
+
+end :: Interval -> Int
+end {start, len} = start + len - 1
+
+areKissing :: Interval -> Interval -> Boolean
+areKissing a b = a.start == end b + 2
+  ||             b.start == end a + 2
+
+data Overlap = SubsetLT
+  | SubsetGT
+  | Equal
+  | Partial Int
+  | Disjoint
+
+derive instance eqOverlap :: Eq Overlap
+
+overlap :: Interval -> Interval -> Overlap
+overlap a b =
+  if a == b then Equal else
+  todo
+
+disjoint :: Interval -> Interval -> Boolean
+disjoint a b = overlap a b == Disjoint
+
+
+--      |------|
+-- |----|      |1j
+-- |           |2
+-- |       B   |3
+-- | A  |      |4
+-- |    |------|5
+-- |----|  C   |6
+--      |      |7
+--      |------|8
+
+unsafeIndex :: forall a. Array a -> Int -> a
+unsafeIndex a i = unsafePartial $ Array.unsafeIndex a i
+
+blockAdjacency :: Array Block -> Array {a :: Int, b :: Int}
+blockAdjacency blocks = do
+  let n = Array.length blocks -1
+  a <- Array.range 0 n
+  b <- Array.range 0 n
+  guard $ a /= b
+  guard $ areAdjacent (unsafeIndex blocks a) (unsafeIndex blocks b)
+  pure {a,b}
 
 
 
