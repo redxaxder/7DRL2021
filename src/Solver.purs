e34b307cbee0a21596a7fbb06acf8ce1f2a4d9cb
redxaxder <abyaly@gmail.com>
2021-03-13 08:51:31 -0600

    calculate distancemap

--- src/Solver.purs
+++ src/Solver.purs
@@ -7,6 +7,7 @@ import Data.List (List)
 import Data.List as List
 import Data.Array as Array
 import Data.Tuple as Tuple
+import Data.Map as Map
 
 
 
@@ -32,6 +33,29 @@ floodFillStep expand {frontier, expanded} = do
          frontier' = foldr List.Cons tail newVertices
          expanded' = Set.insert head expanded
      pure $ { frontier: frontier', expanded: expanded' }
+
+distanceMap :: forall v. Ord v => v -> (v -> Array v) -> Map v Int
+distanceMap start expand = _.expanded $ flip loop
+  { frontier: List.singleton {v:start,d:0}
+  , expanded: Map.empty
+  } $ distanceMapStep expand
+
+type DistanceMapStepData v =
+  { frontier :: List {v::v,d::Int}
+  , expanded :: Map v Int
+  }
+
+distanceMapStep :: forall v.
+  Ord v => (v -> Array v) -> DistanceMapStepData v -> Maybe (DistanceMapStepData v)
+distanceMapStep expand {frontier, expanded} = do
+  { head: head@{v,d}, tail } <- List.uncons frontier
+  let newNodes = expand v
+               # Array.filter (not <<< flip Map.member expanded)
+               # map (\w -> {v:w,d:d+1})
+      frontier' = foldr List.Cons tail newNodes
+      expanded' = Map.insert v d expanded
+  pure $ { frontier: frontier', expanded: expanded' }
+
 
 loop :: forall a.  (a -> Maybe a) -> a -> a
 loop next seed = maybe seed (loop next) (next seed)
